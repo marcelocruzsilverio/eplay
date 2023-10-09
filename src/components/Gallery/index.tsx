77ebd354d8b9770a49040a20a7d174f276f1a3c2
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2023-10-09 12:27:49 -0300

    Criando e componentizando a section e gallery

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,67 @@
+import Section from '../Section'
+
+import spider from '../../assets/images/banner-homem-aranha.png'
+import hogwarts from '../../assets/images/fundo_hogwarts.png'
+import zoom from '../../assets/images/zoom.png'
+import play from '../../assets/images/play.png'
+import { Action, Item, Items } from './styles'
+
+type GalleryItem = {
+  type: 'image' | 'video'
+  url: string
+}
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: spider
+  },
+  {
+    type: 'image',
+    url: hogwarts
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/uHGShqcAHlQ'
+  }
+]
+
+type Props = {
+  defaultCover: string
+  name: string
+}
+
+const Gallery = ({ defaultCover, name }: Props) => {
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GalleryItem) => {
+    if (item.type === 'image') return zoom
+    return play
+  }
+
+  return (
+    <Section title="Galeria" background="black">
+      <Items>
+        {mock.map((media, index) => (
+          <Item key={media.url}>
+            <img
+              src={getMediaCover(media)}
+              alt={`Mídia ${index + 1} de ${name}`}
+            />
+            <Action>
+              <img
+                src={getMediaIcon(media)}
+                alt="clique para maximizar a mídia"
+              />
+            </Action>
+          </Item>
+        ))}
+      </Items>
+    </Section>
+  )
+}
+
+export default Gallery
